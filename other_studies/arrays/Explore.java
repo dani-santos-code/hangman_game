e499fe7dd4a528df0142c4f832bcf612a9acb2e7
Dani Santos <santos.adaniele@gmail.com>
2019-06-24 17:11:24 +0200

    more on looping through arrays

--- other_studies/arrays/Explore.java
+++ other_studies/arrays/Explore.java
@@ -1,10 +1,22 @@
+import com.sun.jndi.url.iiop.iiopURLContextFactory;
+
 public class Explore {
     public static void main(String[] args) {
         // Create a new friends array using the array literal shortcut
         // Include Ben, Alena, Pasan
         String[] friends = { "Ben", "Alena", "Pasan" };
+        // Enhanced foor loop
         for (String friend : friends) {
             System.out.printf("Hey, %s! The movie starts at 7. C U there! %n", friend);
+        }
+        //Standard format
+        for (int i = 0; i < friends.length; i++) {
+            System.out.printf("Hey, %s! The movie starts at 7. C U there! %n", friends[i]);
+        }
+        int[] bensScoreCard = {1, 2, 1, 5, 2, 4, 4, 4, 3, 6, 1, 2, 5, 4, 3, 2, 6, 3};
+        for (int i = 0; i < bensScoreCard.length; i++) {
+            System.out.printf("Hole number #%d: %d %n", i + 1, bensScoreCard[i]);
+
         }
     }
 }
