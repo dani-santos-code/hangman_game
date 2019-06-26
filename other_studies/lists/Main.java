eb74b9e46d1562f3b7ff27e30a59588b526653dd
Dani Santos <santos.adaniele@gmail.com>
2019-06-26 15:03:43 +0200

    practice list collections in java

---
+++ other_studies/lists/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+        public static void main(String[] args) {
+            List<String> groceryLine = new ArrayList<>();
+            groceryLine.add("Jerome");
+            groceryLine.add("Beth");
+            groceryLine.add("Sam");
+            System.out.println(groceryLine);
+            // groceryLine.remove(1);
+            // System.out.println(groceryLine);
+            String jerome = groceryLine.get(0);
+            System.out.println(jerome);
+
+            int joshIndex = groceryLine.indexOf("Josh");
+            int samIndex = groceryLine.indexOf("Sam");
+            System.out.println(joshIndex + samIndex);
+
+            // System.out.println(groceryLine.size());
+
+            // for (String name : groceryLine) {
+            //     System.out.println(name);
+            // }
+        }
+}
